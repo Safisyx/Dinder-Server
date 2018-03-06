85f8afe142b3279a09148b573eddfa2a0e6471e4
evagolabek <golabek.eva@gmail.com>
2018-03-06 15:26:36 +0100

    adjusted folders with models and routers

--- breeds/router.js
+++ breeds/router.js
@@ -1,5 +1,5 @@
 const Router = require('express').Router
-const Product = require('./model')
+const Breed = require('./model')
 
 const router = new Router()
 module.exports = router
@@ -14,5 +14,21 @@ router.get('/breeds', (req, res) => {
     .catch(err => {
       res.status(500)
       res.json({message: 'Something went wrong'})
+    })
+})
+
+router.get('/breeds/:id', (req, res) => {
+  Breed.findById(req.params.id)
+    .then(result => {
+      if (result) {
+        res.json(result)
+      } else {
+        res.status(404)
+        res.json({ message: 'Not Found' })
+      }
+    })
+    .catch(err => {
+      res.status(500)
+      res.json({ message: 'There was an error' })
     })
 })
